7d30aa6fb943975582724ca22ab39745611b6b68
Schwinebuer08 <135615709+Schwinebuer08@users.noreply.github.com>
2025-04-14 01:13:30 +0200

    Update to V1.4

---
+++ scripte/Oseikomi/counterb.js
@@ -0,0 +1,41 @@
+let counterb = 0;
+let timer_onb = 0;
+let timeoutb;
+
+function timeCountb() {
+  counterb = Math.min(counterb + 1, maxOseikomi);
+  timeoutb = setTimeout(timeCountb, 1000);
+  document.getElementById("counterb").innerHTML =
+    "Oseikomi:   " + counterb + "   Sekunden";
+  if (counterb == Math.round(maxOseikomi / 4)) {
+    blauyuko();
+  } else if (counterb == Math.round(maxOseikomi / 2)) {
+    punkteb = punkteb - yuko;
+    blauwaz();
+  } else if (counterb == maxOseikomi) {
+    punkteb = punkteb - wazari;
+    blauipp();
+    stopCountb();
+    stopCountdown();
+  }
+}
+
+function startCountb() {
+  if (!timer_onb) {
+    timer_onb = 1;
+    startCountdown();
+    setTimeout(timeCountb, 1000);
+  }
+}
+
+function stopCountb() {
+  clearTimeout(timeoutb);
+  timer_onb = 0;
+}
+
+function resetb() {
+  stopCountb();
+  counterb = counterb - counterb;
+  document.getElementById("counterb").innerHTML =
+    "Oseikomi:   " + counterb + "   Sekunden";
+}
---
+++ scripte/Oseikomi/counterw.js
@@ -0,0 +1,41 @@
+let counterw = 0;
+let timer_onw = 0;
+let timeoutw;
+
+function timeCountw() {
+  counterw = Math.min(counterw + 1, maxOseikomi);
+  timeoutw = setTimeout(timeCountw, 1000);
+  document.getElementById("counterw").innerHTML =
+    "Oseikomi:   " + counterw + "   Sekunden";
+  if (counterw == Math.round(maxOseikomi / 4)) {
+    weißyuko();
+  } else if (counterw == Math.round(maxOseikomi / 2)) {
+    punktew = punktew - yuko;
+    weißwaz();
+  } else if (counterw == maxOseikomi) {
+    punktew = punktew - wazari;
+    weißipp();
+    stopCountw();
+    stopCountdown();
+  }
+}
+
+function startCountw() {
+  if (!timer_onw) {
+    timer_onw = 1;
+    startCountdown();
+    setTimeout(timeCountw, 1000);
+  }
+}
+
+function stopCountw() {
+  clearTimeout(timeoutw);
+  timer_onw = 0;
+}
+
+function resetw() {
+  stopCountw();
+  counterw = counterw - counterw;
+  document.getElementById("counterw").innerHTML =
+    "Oseikomi:   " + counterw + "   Sekunden";
+}
